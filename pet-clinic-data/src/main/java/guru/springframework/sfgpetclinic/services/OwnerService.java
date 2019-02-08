c8e2cdc8ed46384bbfe04228b83685f4fd0153a5
Monika Maria <monika.kocot9@gmail.com>
2019-02-08 14:29:38 +0100

    Addint Pets to Owners

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
@@ -7,6 +7,7 @@ import java.util.Set;
 public interface OwnerService  extends CrudService<Owner,Long> {
 
     Owner findByLastName(String lastName);
+
 //
 //    Owner findById(Long id);
 //    Owner save(Owner owner);
--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,25 +1,33 @@
 package guru.springframework.sfgpetclinic.bootstrap;
 
 import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.Pet;
+import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.OwnerService;
+import guru.springframework.sfgpetclinic.services.PetTypeService;
 import guru.springframework.sfgpetclinic.services.VetService;
 import guru.springframework.sfgpetclinic.services.map.OwnerServiceMap;
 import guru.springframework.sfgpetclinic.services.map.VetServiceMap;
+import org.apache.tomcat.jni.Local;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader  implements CommandLineRunner{
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
     @Autowired
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     //    public DataLoader() {
@@ -30,10 +38,29 @@ public class DataLoader  implements CommandLineRunner{
     @Override
     public void run(String... args) throws Exception {
 
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        dog.setName("Cat");
+        PetType savedCatType = petTypeService.save(cat);
+
+
         Owner owner1 = new Owner();
 //        owner1.setId(1L);
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
+        owner1.setAddress("123 Brickerel");
+        owner1.setCity("Miami");
+        owner1.setTelephone("123123123");
+
+        Pet mikesPet = new Pet();
+        mikesPet.setPetType(savedDogType);
+        mikesPet.setOwner(owner1);
+        mikesPet.setBirthDate(LocalDate.now());
+        mikesPet.setName("Rosco");
+        owner1.getPets().add(mikesPet);
 
         ownerService.save(owner1);
 
@@ -41,10 +68,21 @@ public class DataLoader  implements CommandLineRunner{
 //        owner2.setId(2L);
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
+        owner2.setAddress("123 Brickerel");
+        owner2.setCity("Miami");
+        owner2.setTelephone("321321321");
 
         ownerService.save(owner2);
 
+        Pet fionasPet = new Pet();
+        fionasPet.setPetType(savedCatType);
+        fionasPet.setOwner(owner2);
+        fionasPet.setBirthDate(LocalDate.now());
+        fionasPet.setName("Just Cat");
+        owner2.getPets().add(fionasPet );
+
         System.out.println("Loaded Owners....");
+        System.out.println(owner1.getPets().toString());
 
         Vet vet1 = new Vet();
 //        vet1.setId(1L);
